b462f8c9c8360ff89486c6e0cc8bf36551091dc2
DavisDauk <dauksts30@gmail.com>
2018-11-30 14:40:17 +0000

    d

--- WebApp/dataBase/db.php
+++ WebApp/dataBase/db.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -9,6 +9,6 @@ $conn = new mysqli($servername, $username, $password);
 
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
-} 
+}
 echo "Connected successfully";
 ?>
